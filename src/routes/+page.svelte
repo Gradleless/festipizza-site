e78724e885bd669171a2a9e34d82f6a484153b53
Lyciaa <57102194+Mavenless@users.noreply.github.com>
2024-02-21 20:46:55 +0100

    Mise à jour de la page avec une nouvelle image de restaurant

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
     import Carte from './carte/carte.svelte';
 </script>  
 
-<div class="text-left md:pl-24 max-sm:text-center pb-28" style="background-image: url('/favicon.png');">
+<div class="text-left md:pl-24 max-sm:text-center pb-28 bg-no-repeat bg-cover" style="background-image: url('/resto.jpg');">
     <Heading tag="h1" class="mb-4 pt-32" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">We invest in the world’s potential</Heading>
     <P class="mb-6 text-lg lg:text-xl dark:text-gray-400 text-left md:mr-96 max-sm:text-center">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</P>
     <Button href="/carte" color="green">
