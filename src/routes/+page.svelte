cbfa635a30791fb20b2d36e503157063c56fbc6f
Lyciaa <57102194+Mavenless@users.noreply.github.com>
2024-02-22 00:41:02 +0100

    Mise à jour de la page d'accueil avec du texte en français

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,9 @@
     import Carte from './carte/carte.svelte';
 </script>  
 
-<div class="text-left md:pl-24 max-sm:text-center pb-28 bg-no-repeat bg-[url('/festi.jpg')] bg-left-top bg-blend-darken">
-    <Heading tag="h1" class="mb-4 pt-32" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">We invest in the world’s potential</Heading>
-    <P class="mb-6 text-lg lg:text-2xl text-left md:mr-96 max-sm:text-center">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</P>
+<div class="text-left md:pl-24 max-sm:text-center pb-28 bg-no-repeat bg-[url('/festi.jpg')] bg-left ">
+    <Heading tag="h1" class="mb-4 pt-32" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">Découvrez des saveurs authentiques</Heading>
+    <P class="mb-6 text-lg lg:text-2xl text-left md:mr-96 max-sm:text-center">Chez Festi pizza, nous mettons l'accent sur des ingrédients de qualité et des recettes traditionnelles pour vous offrir une expérience gustative incomparable.</P>
     <Button href="/carte" color="green">
         Regardez la carte
         <ArrowRightOutline class="w-3.5 h-3.5 ms-2" />
