df988ae17ff14d13396f3ad7e509b6c47ec26a33
Lyciaa <57102194+Mavenless@users.noreply.github.com>
2024-02-21 21:58:08 +0100

    Mise à jour de la mise en page et des liens du pied de page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,19 +3,9 @@
     import { Footer, FooterCopyright, FooterLink, FooterLinkGroup, Navbar, NavBrand, NavHamburger, NavUl, NavLi, FooterBrand } from 'flowbite-svelte';
 </script>
 
-<style>
-  /* Add your styles here */
-    * {
-        overflow: hidden;
-    }
-</style>
-
-<svelte:head>
-</svelte:head>
-
 <Navbar color="green" >
     <NavBrand href="/">
-        <img src="/" class="me-3 h-6 sm:h-9" alt="Festi pizza Logo" />
+        <img src="/favicon.jpg" class="me-3 h-6 sm:h-9" alt="Festi pizza Logo" />
         <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Festi pizza</span>
     </NavBrand>
     <NavHamburger  />
@@ -33,15 +23,15 @@
 <div class="mt-12">
     <Footer footerType="logo">
         <div class="sm:flex sm:items-center sm:justify-between">
-            <FooterBrand href="/" src="/" alt="Festi pizza" name="Festi pizza" />
+            <FooterBrand href="/" src="/favicon.jpg" alt="Festi pizza" name="Festi pizza" />
             <FooterLinkGroup ulClass="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
-                <FooterLink href="/">Accueil</FooterLink>
-                <FooterLink href="/carte">Carte</FooterLink> 
-                <FooterLink href="/contact">Contact</FooterLink>
-                <FooterLink href="https://lycia.anhost.fr">A propos</FooterLink>
+                <FooterLink href="/" liClass="me-4 last:me-0 md:me-6 transition ease-in dark:hover:text-green-700" aClass="no-underline">Accueil</FooterLink>
+                <FooterLink href="/carte" liClass="me-4 last:me-0 md:me-6 transition ease-in dark:hover:text-green-700" aClass="no-underline">Carte</FooterLink> 
+                <FooterLink href="/contact" liClass="me-4 last:me-0 md:me-6 transition ease-in dark:hover:text-green-700" aClass="no-underline">Contact</FooterLink>
+                <FooterLink href="https://lycia.anhost.fr" liClass="me-4 last:me-0 md:me-6 transition ease-in dark:hover:text-green-700" aClass="no-underline">A propos</FooterLink>
             </FooterLinkGroup>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <FooterCopyright href="/" by="Festi pizza -" class="dark:hover:no-underline"/>
+        <FooterCopyright href="/" by="Festi pizza -" class="dark:hover:no-underline" aClass="no-underline"/>
     </Footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,9 @@
     import Carte from './carte/carte.svelte';
 </script>  
 
-<div class="text-left md:pl-24 max-sm:text-center pb-28 bg-no-repeat bg-cover" style="background-image: url('/resto.jpg');">
+<div class="text-left md:pl-24 max-sm:text-center pb-28 bg-no-repeat bg-[url('/festi.jpg')] bg-left-top bg-blend-darken">
     <Heading tag="h1" class="mb-4 pt-32" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">We invest in the world’s potential</Heading>
-    <P class="mb-6 text-lg lg:text-xl dark:text-gray-400 text-left md:mr-96 max-sm:text-center">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</P>
+    <P class="mb-6 text-lg lg:text-2xl text-left md:mr-96 max-sm:text-center">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</P>
     <Button href="/carte" color="green">
         Regardez la carte
         <ArrowRightOutline class="w-3.5 h-3.5 ms-2" />
