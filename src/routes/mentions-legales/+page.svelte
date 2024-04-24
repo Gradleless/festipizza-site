b89f34197703cd755c48ca3cb9a7dbc1ee625df9
Lyciaa <57102194+Mavenless@users.noreply.github.com>
2024-04-24 18:55:43 +0200

    Ajout de la page des mentions légales

---
+++ src/routes/mentions-legales/+page.svelte
@@ -0,0 +1,66 @@
+<style>
+    * {
+        color: white;
+    }
+</style>
+
+<section class="container mx-auto py-8">
+    <h1 class="text-3xl">CE SITE N'EST PAS UN SITE OFFICIEL, IL A ÉTÉ CRÉÉ UNIQUEMENT PAR BUT ÉDUCATIF, IL N'APPARTIENT PAS À FESTIPIZZA</h1>
+    <p class="text-sm">Il est donc évident que c'est une fausse carte et de fausses mentions légales. Il n'y a que l'addresse et le numéro de téléphone qui sont véridiques.</p>
+</section>
+
+<section id="cookies_traceurs" class="container mx-auto py-8">
+    <h2 class="text-2xl font-bold mb-4">Cookies et traceurs</h2>
+    <p class="mb-4">Le site internet <a href="https://festipizza-site.vercel.app/" class="text-blue-500">festipizza-site.vercel.app</a> possède un système de mesure d’audience, ainsi qu’une fonction de partage sur les réseaux sociaux.</p>
+    <p class="mb-4">En application de la directive européenne dite « paquet télécom », les internautes doivent être informés et donner leur consentement préalablement à l’insertion de traceurs (plus couramment appelés « cookies »). Les internautes doivent disposer d’une possibilité de choisir de ne pas être tracés lorsqu’ils visitent un site ou utilisent une application. Les éditeurs de sites internet ont donc l’obligation de solliciter au préalable le consentement des utilisateurs.</p>
+    <p class="mb-4">Le refus d’installation d’un cookie peut entraîner l’impossibilité d’accéder à certains services. L’internaute peut toutefois configurer son navigateur internet pour refuser l’installation des cookies.</p>
+</section>
+
+<section id="mesure_audience" class="container mx-auto py-8">
+    <h2 class="text-2xl font-bold mb-4">Mesure d'audience</h2>
+    <p class="mb-4">Les cookies de mesure d’audience sont de petits fichiers qui permettent de connaître et d’analyser les statistiques de trafic sur le site internet : les pages visitées, le nombre de visites, le taux de rebond, la provenance des visites,… Les cookies de mesure d’audience sont totalement anonymes.</p>
+    <p class="mb-4">Sur ce site, c’est la solution Google Analytics qui est utilisée pour mesurer l’audience.</p>
+</section>
+
+<section id="reseaux_sociaux" class="container mx-auto py-8">
+    <h2 class="text-2xl font-bold mb-4">Réseaux sociaux</h2>
+    <p class="mb-4">Les cookies liés aux réseaux sociaux sont associés aux boutons qui facilitent le partage des pages et articles sur les réseaux sociaux.</p>
+</section>
+
+<section id="formulaire_commentaires" class="container mx-auto py-8">
+    <h2 class="text-2xl font-bold mb-4">Formulaire de contact et commentaires</h2>
+    <p class="mb-4">Vous pouvez être amené à nous indiquer votre adresse e-mail lorsque vous remplissez notre formulaire de contact ou déposez un commentaire sur l’un des articles du site <a href="https://festipizza-site.vercel.app/" class="text-blue-500">festipizza-site.vercel.app</a>.</p>
+    <p class="mb-4">En aucun cas, votre adresse e-mail ne sera cédée à des tiers.</p>
+</section>
+
+<section id="moderation_commentaires" class="container mx-auto py-8">
+    <h2 class="text-2xl font-bold mb-4">Modération des commentaires</h2>
+    <p class="mb-4">Le choix de validation d’un commentaire sur le site <a href="https://festipizza-site.vercel.app/" class="text-blue-500">festipizza-site.vercel.app</a> est laissé à l’entière appréciation du responsable de publication. Les commentaires peuvent être supprimés, modifiés et corrigés pour une meilleure compréhension des visiteurs (notamment pour l’orthographe).</p>
+    <p class="mb-4">L’internaute peut signer son commentaire de son nom ou pseudo ou nom de son entreprise. Il peut également renseigné une URL dans le champ « site web ». Ce lien peut ne pas apparaître si il a été jugé que le commentaire n’apportait pas réellement de plus value à l’article. Ceci, même si le commentaire est publié.</p>
+    <p>Voici des exemples de cas où un commentaire peut être modéré ou supprimé :</p>
+    <ul class="list-disc ml-8">
+        <li>Il a été supprimé par l’anti-spam</li>
+        <li>Il n’apporte pas réellement de plus value et n’est pas utile pour les internautes</li>
+        <li>Il est truffé de fautes d’orthographe ou incompréhensible</li>
+        <li>Il semble être déposé uniquement dans un but auto-promotionnel</li>
+        <li>Le mail indiqué est visiblement faux</li>
+        <li>Il est jugé diffamatoire pour un tiers</li>
+    </ul>
+</section>
+
+<section id="newsletter" class="container mx-auto py-8">
+    <h2 class="text-2xl font-bold mb-4">Newsletter</h2>
+    <p class="mb-4">Vous pouvez vous abonner à la newsletter du site. Vous recevez alors automatiquement et gratuitement des newsletters traitant les sujets du site <a href="https://festipizza-site.vercel.app/" class="text-blue-500">festipizza-site.vercel.app</a>.</p>
+    <p class="mb-4">Vous pouvez vous désinscrire à tout moment de la newsletter en cliquant sur le lien de désabonnement présent en bas de chaque newsletter.</p>
+    <p>En aucun cas, votre adresse e-mail ne sera cédée à des tiers.</p>
+</section>
+
+<section id="liens_hypertextes" class="container mx-auto py-8">
+    <h2 class="text-2xl font-bold mb-4">Liens hypertextes</h2>
+    <p class="mb-4">Ce site internet contient un certain nombre de liens hypertextes vers d’autres sites. Cependant, <a href="https://festipizza-site.vercel.app/" class="text-blue-500">festipizza-site.vercel.app</a> n’a pas la possibilité de suivre et vérifier le contenu de ces sites, et n’assumera en conséquence aucune responsabilité de ce fait.</p>
+</section>
+
+<section id="flux_rss" class="container mx-auto py-8">
+    <h2 class="text-2xl font-bold mb-4">Flux RSS</h2>
+    <p class="mb-4">Les flux RSS sont exclusivement destinés aux visiteurs du site pour une utilisation personnelle et ne sauraient en aucun cas servir à alimenter d’autres sites, sauf autorisation écrite préalable de <a href="https://festipizza-site.vercel.app/" class="text-blue-500">festipizza-site.vercel.app</a>.</p>
+</section>
